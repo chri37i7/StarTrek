6e14f9d634c1386e4384c6c1340207f6671713c9
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-01-13 14:48:48 +0100

    Create Name Generation Method

--- StarTrek/Program.cs
+++ StarTrek/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StarTrek
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> validVulcanNames = new List<string>();
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            GenerateVulcanNames();
+
+
+        }
+
+        static void GenerateVulcanNames()
+        {
+            // Arrays for name combinations 
+            string[] firstArray = { "S", "Sp", "Sk", "St", "T" };
+            string[] secondArray = { "a", "e", "i", "o", "u", "y" };
+            string[] thirdArray = { "r", "t", "p", "d", "f", "j", "k", "l", "v", "b", "n", "m" };
+            string[] fourthArray = { "a", "e", "i", "o", "u", "y" };
+            string[] fifthArray = { "q", "p", "k", "ck", "l" };
+
+
+            // Loop until all combinations have been made
+            while(validVulcanNames.Count != 10800)
+            {
+                Random firstRndNum = new Random();
+                int firstNumber = firstRndNum.Next(0, firstArray.Length);
+
+                Random secondRndNum = new Random();
+                int secondNumber = secondRndNum.Next(0, secondArray.Length);
+
+                Random thirdRndNum = new Random();
+                int thirdNumber = thirdRndNum.Next(0, thirdArray.Length);
+
+                Random fourthRndNum = new Random();
+                int fourthNumber = fourthRndNum.Next(0, fourthArray.Length);
+
+                Random fifthRndNum = new Random();
+                int fifthNumber = fifthRndNum.Next(0, fifthArray.Length);
+
+                string name = firstArray[firstNumber] + secondArray[secondNumber] + thirdArray[thirdNumber] + fourthArray[fourthNumber] + fifthArray[fifthNumber];
+
+                if(!validVulcanNames.Contains(name))
+                {
+                    validVulcanNames.Add(name);
+                }
+            }
         }
     }
 }
